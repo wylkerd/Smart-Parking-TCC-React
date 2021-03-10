5219043fabd6f8931eed81237c0f5ff44a334a52
Wylkerd <wylkerd@hotmail.com>
2021-03-10 12:36:57 -0300

    Test

--- src/smartParking.ino
+++ src/smartParking.ino
@@ -1,68 +1,57 @@
-/******************************************************************************/
-/*          Projeto: Protótipo Sensor para Estacionamento em Garagem          */
-/*                     PontoCanal - Ponto Makers                              */
-/******************************************************************************/
+/***********************************/
+/*          SMART PARKING          */
+/***********************************/
+// char result1[] = "{\"corredor\":1,\"sensor\":1,\"status\":\"livre\"}";
 
 //Configuração de Distancia Mínima em centimetros
-const int distancia_carro = 10;
+const int distancia_carro = 2;
 
 //Configurações de Portas do Arduino
 
 //Sensor
-const int TRIG = 3;
-const int ECHO = 2;
+const int TRIG1 = 3;
+const int ECHO1 = 2;
+
+const int TRIG2 = 5;
+const int ECHO2 = 4;
 
 //Demais componentes
-const int ledGreen = 7;
-const int ledRed = 8;
-const int buzzer = 9;
+const int ledGreen1 = 7;
+const int ledRed1 = 8;
 
-// Variaveis para funcionamento do Buzzer
-float seno;
-int frequencia;
+const int ledGreen2 = 1;
+const int ledRed2 = 2;
 
 void setup()
 {
     Serial.begin(9600);
 
-    // Configurações do Sensor
-    pinMode(TRIG, OUTPUT);
-    pinMode(ECHO, INPUT);
+    // Configurações do Sensor1
+    pinMode(TRIG1, OUTPUT);
+    pinMode(ECHO1, INPUT);
 
-    // Configurações do LED
-    pinMode(ledGreen, OUTPUT);
-    pinMode(ledRed, OUTPUT);
+    // Configurações do LED1
+    pinMode(ledGreen1, OUTPUT);
+    pinMode(ledRed1, OUTPUT);
 
-    //Configurações do Buzzer
-    pinMode(buzzer, OUTPUT);
+    // Configurações do Sensor2
+    pinMode(TRIG2, OUTPUT);
+    pinMode(ECHO2, INPUT);
+
+    // Configurações do LED2
+    pinMode(ledGreen2, OUTPUT);
+    pinMode(ledRed2, OUTPUT);
 }
 
 void loop()
 {
-    int distancia = sensor_morcego(TRIG, ECHO);
-
-    if (distancia <= distancia_carro)
-    {
-        Serial.print("Atenção: ");
-        Serial.print(distancia);
-        Serial.println("cm");
-        digitalWrite(ledGreen, LOW);
-        digitalWrite(ledRed, HIGH);
-        tocaBuzzer();
-    }
-    else
-    {
-        Serial.print("Livre: ");
-        Serial.print(distancia);
-        Serial.println("cm");
-        digitalWrite(ledGreen, HIGH);
-        digitalWrite(ledRed, LOW);
-        noTone(buzzer);
-    }
-    delay(100);
+    char result1 = (verificacao1() == 1) ? 'livre' : 'ocupada');
+    Serial.println(result1);
+    char result2 = (verificacao2() == 1) ? 'livre' : 'ocupada');
+    Serial.println(result2);
 }
 
-int sensor_morcego(int pinotrig, int pinoecho)
+int sensor_morcego1(int pinotrig, int pinoecho)
 {
     digitalWrite(pinotrig, LOW);
     delayMicroseconds(2);
@@ -73,14 +62,165 @@ int sensor_morcego(int pinotrig, int pinoecho)
     return pulseIn(pinoecho, HIGH) / 58;
 }
 
-//Função para execução do Alarme Sonoro
-void tocaBuzzer()
+int verificacao1()
 {
-    for (int x = 0; x < 180; x++)
+    int distancia1 = sensor_morcego1(TRIG1, ECHO1);
+
+    if (distancia1 <= distancia_carro)
     {
-        seno = (sin(x * 3.1416 / 180));
-        frequencia = 2000 + (int(seno * 1000));
-        tone(buzzer, frequencia);
-        delay(2);
+        digitalWrite(ledGreen1, LOW);
+        digitalWrite(ledRed1, HIGH);
+        return 0;
     }
+    else
+    {
+        digitalWrite(ledGreen1, HIGH);
+        digitalWrite(ledRed1, LOW);
+        return 1;
+    }
+    delay(100);
 }
+
+/*************************************************************************/
+
+int sensor_morcego2(int pinotrig, int pinoecho)
+{
+    digitalWrite(pinotrig, LOW);
+    delayMicroseconds(2);
+    digitalWrite(pinotrig, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(pinotrig, LOW);
+
+    return pulseIn(pinoecho, HIGH) / 58;
+}
+
+int verificacao2()
+{
+    int distancia2 = sensor_morcego2(TRIG2, ECHO2);
+
+    if (distancia2 <= distancia_carro)
+    {
+        digitalWrite(ledGreen2, LOW);
+        digitalWrite(ledRed2, HIGH);
+        return 0;
+    }
+    else
+    {
+        digitalWrite(ledGreen2, HIGH);
+        digitalWrite(ledRed2, LOW);
+        return 1;
+    }
+    delay(100);
+}
+
+// /***********************************/
+// /*          SMART PARKING          */
+// /***********************************/
+// #include <tuple>
+// //Configuração de Distancia Mínima em centimetros
+// const int distancia_carro = 2;
+
+// //Configurações de Portas do Arduino
+
+// //Sensor
+// const int TRIG1 = 3;
+// const int ECHO1 = 2;
+
+// const int TRIG2 = 4;
+// const int ECHO2 = 5;
+
+// //Demais componentes
+// const int ledGreen1 = 7;
+// const int ledRed1 = 8;
+
+// const int ledGreen2 = 9;
+// const int ledRed2 = 10;
+
+// void setup()
+// {
+//     Serial.begin(9600);
+
+//     // Configurações do Sensor1
+//     pinMode(TRIG1, OUTPUT);
+//     pinMode(ECHO1, INPUT);
+
+//     // Configurações do LED1
+//     pinMode(ledGreen1, OUTPUT);
+//     pinMode(ledRed1, OUTPUT);
+
+//     // Configurações do Sensor2
+//     pinMode(TRIG2, OUTPUT);
+//     pinMode(ECHO2, INPUT);
+
+//     // Configurações do LED2
+//     pinMode(ledGreen2, OUTPUT);
+//     pinMode(ledRed2, OUTPUT);
+// }
+
+// void loop()
+// {
+//     verificacao1();
+//     verificacao2();
+// }
+
+// int sensor_morcego1(int pinotrig, int pinoecho)
+// {
+//     digitalWrite(pinotrig, LOW);
+//     delayMicroseconds(2);
+//     digitalWrite(pinotrig, HIGH);
+//     delayMicroseconds(10);
+//     digitalWrite(pinotrig, LOW);
+
+//     return pulseIn(pinoecho, HIGH) / 58;
+// }
+
+// std::tuple<char, int> verificacao1()
+// {
+//     int distancia1 = sensor_morcego1(TRIG1, ECHO1);
+
+//     if (distancia1 <= distancia_carro)
+//     {
+//         digitalWrite(ledGreen1, LOW);
+//         digitalWrite(ledRed1, HIGH);
+//         return {"Sensor 1", 0};
+//     }
+//     else
+//     {
+//         digitalWrite(ledGreen1, HIGH);
+//         digitalWrite(ledRed1, LOW);
+//         return {"Sensor 1", 1};
+//     }
+//     delay(100);
+// }
+
+// /*************************************************************************/
+
+// int sensor_morcego2(int pinotrig, int pinoecho)
+// {
+//     digitalWrite(pinotrig, LOW);
+//     delayMicroseconds(2);
+//     digitalWrite(pinotrig, HIGH);
+//     delayMicroseconds(10);
+//     digitalWrite(pinotrig, LOW);
+
+//     return pulseIn(pinoecho, HIGH) / 58;
+// }
+
+// std::tuple<int, int> verificacao2()
+// {
+//     int distancia2 = sensor_morcego2(TRIG2, ECHO2);
+
+//     if (distancia2 <= distancia_carro)
+//     {
+//         digitalWrite(ledGreen2, LOW);
+//         digitalWrite(ledRed2, HIGH);
+//         return {"Sensor 2", 0};
+//     }
+//     else
+//     {
+//         digitalWrite(ledGreen2, HIGH);
+//         digitalWrite(ledRed2, LOW);
+//         return {"Sensor 2", 1};
+//     }
+//     delay(100);
+// }
