e3923ba481d9844dd7deb02f9ca624688c90f3d8
Wylkerd <wylkerd@hotmail.com>
2021-03-11 11:55:16 -0300

    test 3

--- src/smartParking.ino
+++ src/smartParking.ino
@@ -25,16 +25,16 @@ const int rows = 10;
 const int columns = 3;
 
 char status[rows][columns] = {
-    {1, 1, (char)'livre'},
-    {1, 1, (char)'livre'},
-    {1, 1, (char)'livre'},
-    {1, 1, (char)'livre'},
-    {1, 1, (char)'livre'},
-    {1, 1, (char)'livre'},
-    {1, 1, (char)'livre'},
-    {1, 1, (char)'livre'},
-    {1, 1, (char)'livre'},
-    {1, 1, (char)'livre'}};
+    {(char)'1', (char)'1', (char)'livre'},
+    {(char)'1', (char)'2', (char)'livre'},
+    {(char)'1', (char)'3', (char)'livre'},
+    {(char)'1', (char)'4', (char)'livre'},
+    {(char)'1', (char)'5', (char)'livre'},
+    {(char)'1', (char)'6', (char)'livre'},
+    {(char)'1', (char)'7', (char)'livre'},
+    {(char)'1', (char)'8', (char)'livre'},
+    {(char)'1', (char)'9', (char)'livre'},
+    {(char)'1', (char)'10', (char)'livre'}};
 
 void setup()
 {
@@ -59,17 +59,13 @@ void setup()
 
 void loop()
 {
-    // char result1 = (verificacao1() == 1) ? 'livre' : 'ocupada');
-    // Serial.println(result1);
-    // char result2 = (verificacao2() == 1) ? 'livre' : 'ocupada');
-    // Serial.println(result2);
 
     char result = (verificacao1() == 1) ? status[0][2] = (char)'livre' : status[0][2] = (char)'ocupada';
     result = (verificacao2() == 1) ? status[1][2] = (char)'livre' : status[1][2] = (char)'ocupada';
     //Serial.print(status);
     for (byte i = 0; i < sizeof(status); i = i + 1)
     {
-        Serial.write((char)status[i]);
+        Serial.println(status[i]);
     }
 }
 
@@ -134,115 +130,3 @@ int verificacao2()
     }
     delay(100);
 }
-
-// /***********************************/
-// /*          SMART PARKING          */
-// /***********************************/
-// #include <tuple>
-// //Configuração de Distancia Mínima em centimetros
-// const int distancia_carro = 2;
-
-// //Configurações de Portas do Arduino
-
-// //Sensor
-// const int TRIG1 = 3;
-// const int ECHO1 = 2;
-
-// const int TRIG2 = 4;
-// const int ECHO2 = 5;
-
-// //Demais componentes
-// const int ledGreen1 = 7;
-// const int ledRed1 = 8;
-
-// const int ledGreen2 = 9;
-// const int ledRed2 = 10;
-
-// void setup()
-// {
-//     Serial.begin(9600);
-
-//     // Configurações do Sensor1
-//     pinMode(TRIG1, OUTPUT);
-//     pinMode(ECHO1, INPUT);
-
-//     // Configurações do LED1
-//     pinMode(ledGreen1, OUTPUT);
-//     pinMode(ledRed1, OUTPUT);
-
-//     // Configurações do Sensor2
-//     pinMode(TRIG2, OUTPUT);
-//     pinMode(ECHO2, INPUT);
-
-//     // Configurações do LED2
-//     pinMode(ledGreen2, OUTPUT);
-//     pinMode(ledRed2, OUTPUT);
-// }
-
-// void loop()
-// {
-//     verificacao1();
-//     verificacao2();
-// }
-
-// int sensor_morcego1(int pinotrig, int pinoecho)
-// {
-//     digitalWrite(pinotrig, LOW);
-//     delayMicroseconds(2);
-//     digitalWrite(pinotrig, HIGH);
-//     delayMicroseconds(10);
-//     digitalWrite(pinotrig, LOW);
-
-//     return pulseIn(pinoecho, HIGH) / 58;
-// }
-
-// std::tuple<char, int> verificacao1()
-// {
-//     int distancia1 = sensor_morcego1(TRIG1, ECHO1);
-
-//     if (distancia1 <= distancia_carro)
-//     {
-//         digitalWrite(ledGreen1, LOW);
-//         digitalWrite(ledRed1, HIGH);
-//         return {"Sensor 1", 0};
-//     }
-//     else
-//     {
-//         digitalWrite(ledGreen1, HIGH);
-//         digitalWrite(ledRed1, LOW);
-//         return {"Sensor 1", 1};
-//     }
-//     delay(100);
-// }
-
-// /*************************************************************************/
-
-// int sensor_morcego2(int pinotrig, int pinoecho)
-// {
-//     digitalWrite(pinotrig, LOW);
-//     delayMicroseconds(2);
-//     digitalWrite(pinotrig, HIGH);
-//     delayMicroseconds(10);
-//     digitalWrite(pinotrig, LOW);
-
-//     return pulseIn(pinoecho, HIGH) / 58;
-// }
-
-// std::tuple<int, int> verificacao2()
-// {
-//     int distancia2 = sensor_morcego2(TRIG2, ECHO2);
-
-//     if (distancia2 <= distancia_carro)
-//     {
-//         digitalWrite(ledGreen2, LOW);
-//         digitalWrite(ledRed2, HIGH);
-//         return {"Sensor 2", 0};
-//     }
-//     else
-//     {
-//         digitalWrite(ledGreen2, HIGH);
-//         digitalWrite(ledRed2, LOW);
-//         return {"Sensor 2", 1};
-//     }
-//     delay(100);
-// }
