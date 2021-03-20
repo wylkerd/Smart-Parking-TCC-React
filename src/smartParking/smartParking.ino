a36fe5ba9a0e582567f2d44e95888bc582496559
Wylkerd <wylkerd@hotmail.com>
2021-03-20 14:14:08 -0300

    changes

--- src/smartParking/smartParking.ino
+++ src/smartParking/smartParking.ino
@@ -5,14 +5,17 @@
 //Configuração de Distancia Mínima em centimetros
 const int distancia_carro = 2;
 
+long duration;
+int distance;
+
 //Configurações de Portas do Arduino
 
 //Sensor
-const int TRIG1 = 21;
-const int ECHO1 = 20;
+const int trigPin1 = 2;
+const int echoPin1 = 40;
 
-const int TRIG2 = 17;
-const int ECHO2 = 16;
+const int trigPin2 = 4;
+const int echoPin2 = 36;
 
 //Demais componentes
 const int ledGreen1 = 19;
@@ -41,16 +44,16 @@ void setup()
     Serial.begin(9600);
 
     // Configurações do Sensor1
-    pinMode(TRIG1, OUTPUT);
-    pinMode(ECHO1, INPUT);
+    pinMode(trigPin1, OUTPUT);
+    pinMode(echoPin1, INPUT);
 
     // Configurações do LED1
     pinMode(ledGreen1, OUTPUT);
     pinMode(ledRed1, OUTPUT);
 
     // Configurações do Sensor2
-    pinMode(TRIG2, OUTPUT);
-    pinMode(ECHO2, INPUT);
+    pinMode(trigPin2, OUTPUT);
+    pinMode(echoPin2, INPUT);
 
     // Configurações do LED2
     pinMode(ledGreen2, OUTPUT);
@@ -60,17 +63,16 @@ void setup()
 void loop()
 {
 
-    (verificacao1() == 1) ? status[0][2] = 1 : status[0][2] = 0;
-    (verificacao2() == 1) ? status[1][2] = 1 : status[1][2] = 0;
-    Serial.print("***********************************************************************");
-    Serial.println("");
-    Serial.print(status[0][0]);Serial.print("\t");Serial.print(status[0][1]);Serial.print("\t");Serial.println(status[0][2]);
-    Serial.print(status[1][0]);Serial.print("\t");Serial.print(status[1][1]);Serial.print("\t");Serial.println(status[1][2]);
+    verificacao1();
+    verificacao2();
     
-//    for (byte i = 0; i < sizeof(status); i = i + 1)
-//    {
-//        Serial.println(status[i][1][2]);
-//    }
+    Serial.println("");
+    Serial.print("Corredor");Serial.print("\t");Serial.print("Vaga");Serial.print("\t");Serial.println("Status");
+    Serial.print(status[0][0]);Serial.print("\t");Serial.print("\t");Serial.print(status[0][1]);Serial.print("\t");Serial.println(status[0][2]);
+    Serial.print(status[1][0]);Serial.print("\t");Serial.print("\t");Serial.print(status[1][1]);Serial.print("\t");Serial.println(status[1][2]);
+    Serial.println("***********************************************************************");
+    delay(100);
+    
 }
 
 int sensor_morcego1(int pinotrig, int pinoecho)
@@ -84,22 +86,40 @@ int sensor_morcego1(int pinotrig, int pinoecho)
     return pulseIn(pinoecho, HIGH) / 58;
 }
 
-int verificacao1()
+//int sensor_morcego1(int pinotrig, int pinoecho)
+//{
+//  // Clears the trigPin
+//  digitalWrite(pinotrig, LOW);
+//  delayMicroseconds(2);
+//  // Sets the trigPin on HIGH state for 10 micro seconds
+//  digitalWrite(pinotrig, HIGH);
+//  delayMicroseconds(10);
+//  digitalWrite(pinotrig, LOW);
+//  // Reads the echoPin, returns the sound wave travel time in microseconds
+//  duration = pulseIn(pinoecho, HIGH);
+//  // Calculating the distance
+//  distance= duration*0.034/2;
+//
+//  return distance;
+//}
+
+void verificacao1()
 {
-    int distancia1 = sensor_morcego1(TRIG1, ECHO1);
+    int distancia1 = sensor_morcego1(trigPin1, echoPin1);
 
     if (distancia1 <= distancia_carro)
     {
         digitalWrite(ledGreen1, LOW);
         digitalWrite(ledRed1, HIGH);
-        return 1;
+        status[0][2] = 0;
     }
     else
     {
         digitalWrite(ledGreen1, HIGH);
         digitalWrite(ledRed1, LOW);
-        return 0;
+        status[0][2] = 1;
     }
+    Serial.println(distancia1);
     delay(100);
 }
 
@@ -116,21 +136,39 @@ int sensor_morcego2(int pinotrig, int pinoecho)
     return pulseIn(pinoecho, HIGH) / 58;
 }
 
-int verificacao2()
+//int sensor_morcego2(int pinotrig, int pinoecho)
+//{
+//  // Clears the trigPin
+//  digitalWrite(pinotrig, LOW);
+//  delayMicroseconds(2);
+//  // Sets the trigPin on HIGH state for 10 micro seconds
+//  digitalWrite(pinotrig, HIGH);
+//  delayMicroseconds(10);
+//  digitalWrite(pinotrig, LOW);
+//  // Reads the echoPin, returns the sound wave travel time in microseconds
+//  duration = pulseIn(pinoecho, HIGH);
+//  // Calculating the distance
+//  distance= duration*0.034/2;
+//
+//  return distance;
+//}
+
+void verificacao2()
 {
-    int distancia2 = sensor_morcego2(TRIG2, ECHO2);
+    int distancia2 = sensor_morcego2(trigPin2, echoPin2);
 
     if (distancia2 <= distancia_carro)
     {
         digitalWrite(ledGreen2, LOW);
         digitalWrite(ledRed2, HIGH);
-        return 1;
+        status[1][2] = 0;
     }
     else
     {
         digitalWrite(ledGreen2, HIGH);
         digitalWrite(ledRed2, LOW);
-        return 0;
+        status[1][2] = 1;
     }
+    Serial.println(distancia2);
     delay(100);
 }
