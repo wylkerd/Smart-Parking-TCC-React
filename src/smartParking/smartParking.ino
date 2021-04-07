a425e0e351c141cf51df1f3f25ad3259d0167134
Wylkerd <wylkerd@hotmail.com>
2021-04-07 11:07:33 -0300

    indented code

--- src/smartParking/smartParking.ino
+++ src/smartParking/smartParking.ino
@@ -67,64 +67,64 @@ int status[rows][columns] = {
     {1, 10, 1}};
 
 void setup()
-{  
-  Serial.begin(9600);
+{
+    Serial.begin(9600);
 
-  // Configurações do Sensor1
-  pinMode(trigPin1, OUTPUT);
-  pinMode(echoPin1, INPUT);
+    // Configurações do Sensor1
+    pinMode(trigPin1, OUTPUT);
+    pinMode(echoPin1, INPUT);
 
-  // Configurações do LED1
-  pinMode(ledGreen1, OUTPUT);
-  pinMode(ledRed1, OUTPUT);
-/////////////////////////////////////////////////
-  // Configurações do Sensor2
-  pinMode(trigPin2, OUTPUT);
-  pinMode(echoPin2, INPUT);
+    // Configurações do LED1
+    pinMode(ledGreen1, OUTPUT);
+    pinMode(ledRed1, OUTPUT);
+    /////////////////////////////////////////////////
+    // Configurações do Sensor2
+    pinMode(trigPin2, OUTPUT);
+    pinMode(echoPin2, INPUT);
 
-  // Configurações do LED2
-  pinMode(ledGreen2, OUTPUT);
-  pinMode(ledRed2, OUTPUT);
-/////////////////////////////////////////////
-  // Configurações do Sensor3
-  pinMode(trigPin3, OUTPUT);
-  pinMode(echoPin3, INPUT);
+    // Configurações do LED2
+    pinMode(ledGreen2, OUTPUT);
+    pinMode(ledRed2, OUTPUT);
+    /////////////////////////////////////////////
+    // Configurações do Sensor3
+    pinMode(trigPin3, OUTPUT);
+    pinMode(echoPin3, INPUT);
 
-  // Configurações do LED3
-  pinMode(ledGreen3, OUTPUT);
-  pinMode(ledRed3, OUTPUT);
-//////////////////////////////////////////////////
-  // Configurações do Sensor4
-  pinMode(trigPin4, OUTPUT);
-  pinMode(echoPin4, INPUT);
+    // Configurações do LED3
+    pinMode(ledGreen3, OUTPUT);
+    pinMode(ledRed3, OUTPUT);
+    //////////////////////////////////////////////////
+    // Configurações do Sensor4
+    pinMode(trigPin4, OUTPUT);
+    pinMode(echoPin4, INPUT);
 
-  // Configurações do LED5
-  pinMode(ledGreen4, OUTPUT);
-  pinMode(ledRed4, OUTPUT);
-//////////////////////////////////////////////////
-  // Configurações do Sensor5
-  pinMode(trigPin5, OUTPUT);
-  pinMode(echoPin5, INPUT);
+    // Configurações do LED5
+    pinMode(ledGreen4, OUTPUT);
+    pinMode(ledRed4, OUTPUT);
+    //////////////////////////////////////////////////
+    // Configurações do Sensor5
+    pinMode(trigPin5, OUTPUT);
+    pinMode(echoPin5, INPUT);
 
-  // Configurações do LED5
-  pinMode(ledGreen5, OUTPUT);
-  pinMode(ledRed5, OUTPUT);
-/////////////////////////////////////////////////
-  // Configurações do Sensor6
-  pinMode(trigPin6, OUTPUT);
-  pinMode(echoPin6, INPUT);
+    // Configurações do LED5
+    pinMode(ledGreen5, OUTPUT);
+    pinMode(ledRed5, OUTPUT);
+    /////////////////////////////////////////////////
+    // Configurações do Sensor6
+    pinMode(trigPin6, OUTPUT);
+    pinMode(echoPin6, INPUT);
 
-  // Configurações do LED6
-  pinMode(ledGreen6, OUTPUT);
-  pinMode(ledRed6, OUTPUT);
-///////////////////////////////////////////////////
-  // Configurações do Sensor7
-  pinMode(trigPin7, OUTPUT);
-  pinMode(echoPin7, INPUT);
+    // Configurações do LED6
+    pinMode(ledGreen6, OUTPUT);
+    pinMode(ledRed6, OUTPUT);
+    ///////////////////////////////////////////////////
+    // Configurações do Sensor7
+    pinMode(trigPin7, OUTPUT);
+    pinMode(echoPin7, INPUT);
 
-  // Configurações do LED7
-  pinMode(ledGreen7, OUTPUT);
-  pinMode(ledRed7, OUTPUT);
+    // Configurações do LED7
+    pinMode(ledGreen7, OUTPUT);
+    pinMode(ledRed7, OUTPUT);
 }
 
 void loop()
@@ -139,21 +139,61 @@ void loop()
     (verifier7() == 1) ? status[6][2] = 1 : status[6][2] = 0;
 
     Serial.println("");
-    Serial.print("Corredor");Serial.print("\t");Serial.print("Vaga");Serial.print("\t");Serial.println("Status");
-    Serial.print(status[0][0]);Serial.print("\t");Serial.print("\t");Serial.print(status[0][1]);Serial.print("\t");Serial.println(status[0][2] == 1 ? "Vaga livre" : "Vaga ocupada");
-    Serial.print(status[1][0]);Serial.print("\t");Serial.print("\t");Serial.print(status[1][1]);Serial.print("\t");Serial.println(status[1][2] == 1 ? "Vaga livre" : "Vaga ocupada");
-    Serial.print(status[2][0]);Serial.print("\t");Serial.print("\t");Serial.print(status[2][1]);Serial.print("\t");Serial.println(status[2][2] == 1 ? "Vaga livre" : "Vaga ocupada");
-    Serial.print(status[3][0]);Serial.print("\t");Serial.print("\t");Serial.print(status[3][1]);Serial.print("\t");Serial.println(status[3][2] == 1 ? "Vaga livre" : "Vaga ocupada");
-    Serial.print(status[4][0]);Serial.print("\t");Serial.print("\t");Serial.print(status[4][1]);Serial.print("\t");Serial.println(status[4][2] == 1 ? "Vaga livre" : "Vaga ocupada");
-    Serial.print(status[5][0]);Serial.print("\t");Serial.print("\t");Serial.print(status[5][1]);Serial.print("\t");Serial.println(status[5][2] == 1 ? "Vaga livre" : "Vaga ocupada");
-    Serial.print(status[6][0]);Serial.print("\t");Serial.print("\t");Serial.print(status[6][1]);Serial.print("\t");Serial.println(status[6][2] == 1 ? "Vaga livre" : "Vaga ocupada");
+    Serial.print("Corredor");
+    Serial.print("\t");
+    Serial.print("Vaga");
+    Serial.print("\t");
+    Serial.println("Status");
+    Serial.print(status[0][0]);
+    Serial.print("\t");
+    Serial.print("\t");
+    Serial.print(status[0][1]);
+    Serial.print("\t");
+    Serial.println(status[0][2] == 1 ? "Vaga livre" : "Vaga ocupada");
+    Serial.print(status[1][0]);
+    Serial.print("\t");
+    Serial.print("\t");
+    Serial.print(status[1][1]);
+    Serial.print("\t");
+    Serial.println(status[1][2] == 1 ? "Vaga livre" : "Vaga ocupada");
+    Serial.print(status[2][0]);
+    Serial.print("\t");
+    Serial.print("\t");
+    Serial.print(status[2][1]);
+    Serial.print("\t");
+    Serial.println(status[2][2] == 1 ? "Vaga livre" : "Vaga ocupada");
+    Serial.print(status[3][0]);
+    Serial.print("\t");
+    Serial.print("\t");
+    Serial.print(status[3][1]);
+    Serial.print("\t");
+    Serial.println(status[3][2] == 1 ? "Vaga livre" : "Vaga ocupada");
+    Serial.print(status[4][0]);
+    Serial.print("\t");
+    Serial.print("\t");
+    Serial.print(status[4][1]);
+    Serial.print("\t");
+    Serial.println(status[4][2] == 1 ? "Vaga livre" : "Vaga ocupada");
+    Serial.print(status[5][0]);
+    Serial.print("\t");
+    Serial.print("\t");
+    Serial.print(status[5][1]);
+    Serial.print("\t");
+    Serial.println(status[5][2] == 1 ? "Vaga livre" : "Vaga ocupada");
+    Serial.print(status[6][0]);
+    Serial.print("\t");
+    Serial.print("\t");
+    Serial.print(status[6][1]);
+    Serial.print("\t");
+    Serial.println(status[6][2] == 1 ? "Vaga livre" : "Vaga ocupada");
     Serial.println("***********************************************************************");
 
     delay(100);
 }
 
 /*SENSOR 1****************************************************/
-int sensor1(int pinotrig, int pinoecho){
+int sensor1(int pinotrig, int pinoecho)
+{
     digitalWrite(pinotrig, LOW);
     delayMicroseconds(2);
     digitalWrite(pinotrig, HIGH);
@@ -165,14 +205,17 @@ int sensor1(int pinotrig, int pinoecho){
     return distance;
 }
 
-int verifier1(){
+int verifier1()
+{
     int distance1 = sensor1(trigPin1, echoPin1);
-    if (distance1 > min_distance){
+    if (distance1 > min_distance)
+    {
         digitalWrite(ledGreen1, LOW);
         digitalWrite(ledRed1, HIGH);
         return 1;
     }
-    else{
+    else
+    {
         digitalWrite(ledGreen1, HIGH);
         digitalWrite(ledRed1, LOW);
         return 0;
@@ -182,7 +225,8 @@ int verifier1(){
 }
 
 /*SENSOR 2****************************************************/
-int sensor2(int pinotrig, int pinoecho){
+int sensor2(int pinotrig, int pinoecho)
+{
     digitalWrite(pinotrig, LOW);
     delayMicroseconds(2);
     digitalWrite(pinotrig, HIGH);
@@ -192,14 +236,17 @@ int sensor2(int pinotrig, int pinoecho){
     return pulseIn(pinoecho, HIGH) / 58;
 }
 
-int verifier2(){
+int verifier2()
+{
     int distance2 = sensor2(trigPin2, echoPin2);
-    if (distance2 > min_distance){
+    if (distance2 > min_distance)
+    {
         digitalWrite(ledGreen2, LOW);
         digitalWrite(ledRed2, HIGH);
         return 1;
     }
-    else{
+    else
+    {
         digitalWrite(ledGreen2, HIGH);
         digitalWrite(ledRed2, LOW);
         return 0;
@@ -209,7 +256,8 @@ int verifier2(){
 }
 
 /*SENSOR 3****************************************************/
-int sensor3(int pinotrig, int pinoecho){
+int sensor3(int pinotrig, int pinoecho)
+{
     digitalWrite(pinotrig, LOW);
     delayMicroseconds(2);
     digitalWrite(pinotrig, HIGH);
@@ -219,12 +267,13 @@ int sensor3(int pinotrig, int pinoecho){
     return pulseIn(pinoecho, HIGH) / 58;
 }
 
-int verifier3(){
+int verifier3()
+{
     int distance3 = sensor3(trigPin3, echoPin3);
     if (distance3 > min_distance)
     {
         digitalWrite(ledGreen3, HIGH);
-        digitalWrite(ledRed3,LOW);
+        digitalWrite(ledRed3, LOW);
         return 1;
     }
     else
@@ -245,7 +294,7 @@ int sensor4(int pinotrig, int pinoecho)
     digitalWrite(pinotrig, HIGH);
     delayMicroseconds(10);
     digitalWrite(pinotrig, LOW);
-    
+
     return pulseIn(pinoecho, HIGH) / 58;
 }
 
@@ -255,7 +304,7 @@ int verifier4()
     if (distance4 > min_distance)
     {
         digitalWrite(ledGreen4, HIGH);
-        digitalWrite(ledRed4,LOW);
+        digitalWrite(ledRed4, LOW);
         return 1;
     }
     else
@@ -276,7 +325,7 @@ int sensor5(int pinotrig, int pinoecho)
     digitalWrite(pinotrig, HIGH);
     delayMicroseconds(10);
     digitalWrite(pinotrig, LOW);
-    
+
     return pulseIn(pinoecho, HIGH) / 58;
 }
 
@@ -286,7 +335,7 @@ int verifier5()
     if (distance5 > min_distance)
     {
         digitalWrite(ledGreen5, HIGH);
-        digitalWrite(ledRed5,LOW);
+        digitalWrite(ledRed5, LOW);
         return 1;
     }
     else
@@ -317,7 +366,7 @@ int verifier6()
     if (distance6 > min_distance)
     {
         digitalWrite(ledGreen6, HIGH);
-        digitalWrite(ledRed6,LOW);
+        digitalWrite(ledRed6, LOW);
         return 1;
     }
     else
@@ -347,7 +396,7 @@ int verifier7()
     if (distance7 > min_distance)
     {
         digitalWrite(ledGreen7, HIGH);
-        digitalWrite(ledRed7,LOW);
+        digitalWrite(ledRed7, LOW);
         return 1;
     }
     else
