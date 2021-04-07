e4ddd5b3f15a0103e82d528be2039a708fcda667
Wylkerd <wylkerd@hotmail.com>
2021-04-07 12:09:37 -0300

    Show in terminal with react

---
+++ src/server/index.tsx
@@ -0,0 +1,27 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static(__dirname + '/public'));
+server.listen(3000, () => console.log('server on port 3000'));
+
+const SerialPort = require('serialport');
+const ReadLine = SerialPort.parsers.Readline;
+
+const port = new SerialPort("COM3", {
+    baudRate: 9600
+});
+const parser = port.pipe(new ReadLine({ delimiter: '\r\n' }));
+
+parser.on('open', function () {
+    console.log('connection is opened');
+});
+
+parser.on('data', function (data) {
+    console.log(data);
+});
+
+parser.on('error', (err) => console.log(err));
+port.on('error', (err) => console.log(err));
