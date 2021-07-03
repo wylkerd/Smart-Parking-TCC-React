e31342e438604fc771fe5f2679919e5152529bf6
wylkerd <wylkerd@hotmail.com>
2021-07-03 16:04:54 -0300

    Server side code

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -1,45 +1,45 @@
-const http = require('http');
-const express = require('express');
+var express = require('express'),
+    app = express(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server),
+    port = 8888;
 
-const app = express();
-const server = http.createServer(app);
-const io = require('socket.io')(server);
+//Server start
+server.listen(port, () => console.log('Server on port ' + port))
 
+//user server
 app.use(express.static(__dirname + '/public'));
-server.listen(3000, () => console.log('server on port 3000'));
+
+io.on('connection', onConnection);
+
+var connectedSocket = null;
+function onConnection(socket){
+    connectedSocket = socket;
+}
 
 const SerialPort = require('serialport');
-const ReadLine = SerialPort.parsers.Readline;
-
-const port = new SerialPort("COM4", {
-    baudRate: 9600
-});
-const parser = port.pipe(new ReadLine({ delimiter: '\r\n' }));
-
-parser.on('open', function () {
-    console.log('connection is opened');
-});
+const Readline = SerialPort.parsers.Readline; 
+const usbport = new SerialPort('COM4');  
+const parser = usbport.pipe(new Readline({ delimiter: '\r\n' })); 
 
 const condition = new Array(7);
-var guard = 0;
-parser.on('data', function (data) {
-
-    for (let index = 0; index < 1; index++) {
-        condition[guard] = data;
-        index++;
-        guard++;
-        if (guard == 7) {
-            guard = 0;
-        }
+let guard = 0;
+parser.on('data', function(data) {
+  for (let index = 0; index < 1; index++) {
+    condition[guard] = data;
+    index++;
+    guard++;
+    if (guard == 7) {
+      guard = 0;
     }
+  }
 
-    if (condition.length == 7) {
-        io.emit('status', condition);
-        console.log(condition);
-        this.condition = [];
-    }
-
+  if (condition.length == 7) {
+    io.emit('status', condition);
+    console.log(condition);
+    this.condition = [];
+  }
 });
 
 parser.on('error', (err) => console.log(err));
-port.on('error', (err) => console.log(err));
+usbport.on('error', (err) => console.log(err));
