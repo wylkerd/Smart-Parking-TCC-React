da610a407a30fb1f358ccf617828cd37c7e73bdb
wylkerd <wylkerd@hotmail.com>
2021-08-11 17:53:57 -0300

    Alteração no título

--- dashboard-tcc/src/components/Navbars/AdminNavbar.tsx
+++ dashboard-tcc/src/components/Navbars/AdminNavbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
             href="#pablo"
             onClick={(e) => e.preventDefault()}
           >
-            Dashboard
+            Olá, seja bem vindos!
           </a>
           {/* Form */}
           {/* <form className="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3">
