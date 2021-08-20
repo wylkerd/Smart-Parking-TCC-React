b3d9ae5ce32cef14c7c4c189fc0e9716bb6bad2e
wylkerd <wylkerd@hotmail.com>
2021-08-20 20:05:22 -0300

    Adicionado botão de saída

--- dashboard-tcc/src/components/Dropdowns/UserDropdown.tsx
+++ dashboard-tcc/src/components/Dropdowns/UserDropdown.tsx
@@ -42,42 +42,17 @@ const UserDropdown = () => {
         }
       >
         <a
-          href="#pablo"
+          href="https://google.com.br"
           className={
             'text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'
           }
           onClick={(e) => e.preventDefault()}
         >
-          Action
+          Sair
         </a>
-        <a
-          href="#pablo"
-          className={
-            'text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'
-          }
-          onClick={(e) => e.preventDefault()}
-        >
-          Another action
-        </a>
-        <a
-          href="#pablo"
-          className={
-            'text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'
-          }
-          onClick={(e) => e.preventDefault()}
-        >
-          Something else here
-        </a>
-        <div className="h-0 my-2 border border-solid border-blueGray-100" />
-        <a
-          href="#pablo"
-          className={
-            'text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'
-          }
-          onClick={(e) => e.preventDefault()}
-        >
-          Seprated link
-        </a>
+
+        {/* <div className="h-0 my-2 border border-solid border-blueGray-100" /> */}
+
       </div>
     </>
   );
